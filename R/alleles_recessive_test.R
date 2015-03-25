84d74c3ab60eaf1c7f5ec28ff92e9c0a4a1d0889
Jeremy McRae <jm33@sanger.ac.uk>
2015-03-25 13:46:45 +0000

    include biallelic functional test

--- R/alleles_recessive_test.R
+++ R/alleles_recessive_test.R
@@ -4,7 +4,8 @@
 #'
 #' @param hgnc HGNC symbol for a gene.
 #' @param chrom chromosome that the gene is on.
-#' @param lof_lof number of probands with inherited biallelic LoF variants in the gene.
+#' @param biallelic_lof number of probands with inherited biallelic LoF variants in the gene.
+#' @param biallelic_func number of probands with inherited biallelic functional variants in the gene.
 #' @param lof_func number of probands with inherited Lof/Func variants in the gene.
 #' @param probands vector of probands who have inherited recessive variants in
 #'     the gene, or NULL.
@@ -15,21 +16,21 @@
 #'
 #' @return a list of P values from tests using the DDD population, the ExAC
 #'     population, under LoF and functional tests.
-analyse_inherited_enrichment <- function(hgnc, chrom, lof_lof, lof_func, probands=NULL, cohort_n=3072, check_last_base=TRUE) {
+analyse_inherited_enrichment <- function(hgnc, chrom, biallelic_lof, biallelic_func, lof_func, probands=NULL, cohort_n=3072, check_last_base=TRUE) {
     
     cat("extracting ddd frequencies\n")
     ddd = try(get_ddd_variants_for_gene(hgnc, chrom, probands, check_last_base=check_last_base), silent=TRUE)
     if (class(ddd) != "try-error") {
         ddd = get_cumulative_frequencies(ddd)
-        ddd = test_enrichment(ddd, lof_lof, lof_func, cohort_n)
+        ddd = test_enrichment(ddd, biallelic_lof, biallelic_func, lof_func, cohort_n)
     } else {
-        ddd=list(lof=NA, func=NA, lof_p=NA, func_p=NA)
+        ddd=list(lof=NA, func=NA, biallelic_lof_p=NA, lof_func_p=NA, biallelic_func_p=NA)
     }
     
     cat("extracting ExAC frequencies\n")
     exac = get_exac_variants_for_gene(hgnc, chrom, check_last_base=check_last_base)
     exac = get_cumulative_frequencies(exac)
-    exac = test_enrichment(exac, lof_lof, lof_func, cohort_n)
+    exac = test_enrichment(exac, biallelic_lof, biallelic_func, lof_func, cohort_n)
     
     p_values = list(ddd=ddd, exac=exac)
     
@@ -38,22 +39,25 @@ analyse_inherited_enrichment <- function(hgnc, chrom, lof_lof, lof_func, proband
 
 #' test for enrichment of inherited variants
 #'
-#' @param freq list of cumulative frequencies of variation in populations for
+#' @param freq list of cumulative frequencies of variation in a population for
 #'     rare LoF variants, and rare functional variants.
 #' @param biallelic_lof number of probands with inherited Lof/LoF variants in the gene.
+#' @param biallelic_func number of probands with inherited func/func variants in the gene.
 #' @param lof_func number of probands with inherited Lof/Func variants in the gene.
 #' @param cohort_n number of probands in population.
 #' @export
 #'
 #' @return a list of P values from tests, under LoF and functional tests.
-test_enrichment <- function(freq, biallelic_lof, lof_func, cohort_n) {
+test_enrichment <- function(freq, biallelic_lof, biallelic_func, lof_func, cohort_n) {
     lof_rate = freq$lof * freq$lof
     lof_func_rate = freq$lof * (freq$lof + freq$functional)
+    func_rate = freq$func * freq$func
     
     # get the probability of getting more than or equal to the number of
     # observed inherited events
-    freq$lof_p = pbinom(biallelic_lof - 1, cohort_n, prob=lof_rate, lower.tail=FALSE)
-    freq$func_p = pbinom(lof_func + biallelic_lof - 1, cohort_n, prob=lof_func_rate, lower.tail=FALSE)
+    freq$biallelic_lof_p = pbinom(biallelic_lof - 1, cohort_n, prob=lof_rate, lower.tail=FALSE)
+    freq$lof_func_p = pbinom(lof_func + biallelic_lof - 1, cohort_n, prob=lof_func_rate, lower.tail=FALSE)
+    freq$biallelic_func_p = pbinom(biallelic_func - 1, cohort_n, prob=func_rate, lower.tail=FALSE)
     
     return(freq)
 }
