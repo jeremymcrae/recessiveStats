03d5f86753f0828b2088c871a97692c5b8e3b207
Jeremy McRae <jm33@sanger.ac.uk>
2015-05-11 13:54:17 +0100

    count biallelic synonymous families per gene

--- scripts/count_recessive_familys_per_gene.R
+++ scripts/count_recessive_familys_per_gene.R
@@ -10,12 +10,12 @@ DATAFREEZE_DIR = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2014-11-04"
 TRIOS_PATH = file.path(DATAFREEZE_DIR, "family_relationships.txt")
 PHENOTYPES_PATH = file.path(DATAFREEZE_DIR, "phenotypes_and_patient_info.txt")
 SANGER_IDS_PATH = file.path(DATAFREEZE_DIR, "person_sanger_decipher.txt")
-CLINICALFILTER_OUTPUT = "/nfs/users/nfs_j/jm33/clinical_reporting_output/clinical_reporting.2015-04-17.last_base_rule.txt"
+CLINICALFILTER_OUTPUT = "/nfs/users/nfs_j/jm33/clinical_reporting_output/clinical_reporting.2015-05-11.synonymous_variants.txt"
 LIKELY_DIAGNOSED = "/lustre/scratch113/projects/ddd/users/jm33/ddd_likely_diagnosed.txt"
 
 DATA_DIR = "/nfs/users/nfs_j/jm33/apps/recessiveStats/data-raw"
-OUTPUT_COUNTS_PATH = file.path(DATA_DIR, "recessive_counts_by_gene.last_base_rule.txt")
-OUTPUT_PROBANDS_JSON = file.path(DATA_DIR, "recessive_probands_by_gene.last_base_rule.json")
+OUTPUT_COUNTS_PATH = file.path(DATA_DIR, "recessive_counts_by_gene.silent.txt")
+OUTPUT_PROBANDS_JSON = file.path(DATA_DIR, "recessive_probands_by_gene.silent.json")
 
 open_variants <- function() {
     diagnosed = read.table(LIKELY_DIAGNOSED, header=TRUE, stringsAsFactors=FALSE)
@@ -96,6 +96,13 @@ count_for_uncertain_probands <- function(variants, uncertain) {
         gene = variants[variants$proband == row$proband & variants$gene == row$gene, ]
         func_genotypes = gene$trio_genotype[gene$functional == "func"]
         lof_genotypes = gene$trio_genotype[gene$functional == "lof"]
+        silent_genotypes = gene$trio_genotype[gene$functional == "silent"]
+        
+        # If we have two "functional" variants with differing genotypes, then
+        # this could be a func/func category.
+        if (length(table(silent_genotypes)) > 1) {
+            status[row_num] = "silent_silent"
+        }
         
         # If we have two "functional" variants with differing genotypes, then
         # this could be a func/func category.
@@ -138,6 +145,25 @@ count_homozygous_lofs_per_gene <- function(variants) {
     return(single_lofs)
 }
 
+# find the LoFs recessively inherited as variants on their own
+count_homozygous_silent_per_gene <- function(variants) {
+    single_lofs = variants[grepl("single_variant", variants$result) &
+        grepl("Biallelic", variants$inheritance) &
+        variants$functional == "silent", ]
+        
+    # get the homozygous recessive single LoFs,
+    single_lofs = single_lofs[sapply(strsplit(single_lofs$trio_genotype, "/"), "[", 1) == "2", ]
+    
+    # and only count one per gene (ie if a proband has two homozygous recessive
+    # LoFs in a gene, we count that as a single proband)
+    single_lofs = single_lofs[!duplicated(single_lofs[, c("proband", "chrom", "gene")]), ]
+    
+    single_lofs = single_lofs[, c("proband", "gene", "chrom")]
+    single_lofs$category = "homozygous_silent"
+    
+    return(single_lofs)
+}
+
 # find compound hets in genes
 count_compound_hets <- function(variants) {
     # get only the compound het variants
@@ -147,6 +173,7 @@ count_compound_hets <- function(variants) {
     # func/func
     probands = cast(compound_hets, proband + gene + chrom ~ functional, value="result", length)
     probands$category = NA
+    probands$category[probands$func == 0 & probands$silent > 1] = "silent_silent"
     probands$category[probands$func == 0 & probands$lof > 1] = "lof_lof"
     probands$category[probands$func > 1 & probands$lof == 0] = "func_func"
     probands$category[probands$func == 1 & probands$lof == 1] = "lof_func"
@@ -163,17 +190,17 @@ count_compound_hets <- function(variants) {
 
 count_probands_per_gene <- function(variants) {
     
-    homozygous_lof = count_homozygous_lofs_per_gene(variants)
+    homozygous_synonymous = count_homozygous_silent_per_gene(variants)
     compound_hets = count_compound_hets(variants)
     
-    probands = rbind(homozygous_lof, compound_hets)
+    probands = rbind(homozygous_synonymous, compound_hets)
     counts = cast(probands, gene + chrom ~ category, value="proband", length)
     
     counts[is.na(counts)] = 0
-    counts$lof_sum = rowSums(counts[, c("lof_lof", "homozygous_lof")])
+    counts$silent_sum = rowSums(counts[, c("silent_silent", "homozygous_silent")])
     
     # only include recurrent genes
-    counts = counts[rowSums(counts[, c("lof_func", "lof_sum")]) > 1, ]
+    counts = counts[counts$silent_sum > 1, ]
     
     # drop the func/func probands, since there are far too many of them, and we
     # currently don't use them in analyses
@@ -190,11 +217,13 @@ set_functional_state <- function(variants) {
     missense_cq = c("missense_variant", "initiator_codon_variant", "stop_lost",
         "inframe_deletion", "inframe_insertion", "splice_region_variant",
         "coding_sequence_variant")
+    synonymous_cq = c("synonymous_variant")
     
     # define the two functional categories, loss-of-function versus functional
     variants$functional = NA
     variants$functional[grepl(paste(missense_cq, collapse="|"), variants$consequence)] = "func"
     variants$functional[grepl(paste(lof_cq, collapse="|"), variants$consequence)] = "lof"
+    variants$functional[grepl(paste(synonymous_cq, collapse="|"), variants$consequence)] = "silent"
     
     return(variants)
 }
