20787d3b602a390b99acb039f2a28f6cf37257a7
Jeremy McRae <jm33@sanger.ac.uk>
2015-05-22 12:53:15 +0100

    update script for new BCF path

--- scripts/autozygosity/proband_autozygosity.R
+++ scripts/autozygosity/proband_autozygosity.R
@@ -8,6 +8,9 @@ parser$add_argument("--proband", help="HGNC symbol for gene to analyse.")
 
 args = parser$parse_args()
 
-bcf = "/lustre/scratch113/projects/ddd/users/jm33/ddd_4k.bcf"
+bcf = "/lustre/scratch113/projects/ddd/users/jm33/ddd_4k.bcftools.bcf"
 proband = check_sample_autozygosity_genome_wide(bcf, args$proband)
 write.table(proband, file=file.path("data-raw", args$proband) sep="\t", row.names=FALSE, quote=FALSE)
+
+
+# /software/hgi/pkglocal/bcftools-1.0/bin/bcftools roh --biallelic-sites --estimate-AF all --GTs-only 30 --skip-indels --samples DDDP110714 /lustre/scratch113/projects/ddd/users/jm33/ddd_4k.bcftools.bcf
