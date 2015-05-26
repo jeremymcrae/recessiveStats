ec7bfcab886c0900087fdf5fd80aa273cb00c717
Jeremy McRae <jm33@sanger.ac.uk>
2015-05-26 13:36:42 +0100

    fix bug with missing variable

--- R/alleles_recessive_test.R
+++ R/alleles_recessive_test.R
@@ -131,7 +131,7 @@ test_enrichment_across_multiple_populations <- function(exac, biallelic_lof, bia
     
     p_values = list(lof=NA, functional=NA)
     p_values$biallelic_lof_p = sum_combo_tests(exac, cohort_n,
-        , biallelic_lof_enrichment)
+        biallelic_lof, biallelic_lof_enrichment)
     p_values$biallelic_func_p = sum_combo_tests(exac, cohort_n,
         biallelic_func_combos, biallelic_func_enrichment)
     p_values$lof_func_p = sum_combo_tests(exac, cohort_n,
