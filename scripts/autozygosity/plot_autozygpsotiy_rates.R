7857d706774e796714b5d05c052ee51549b1efd7
Jeremy McRae <jm33@sanger.ac.uk>
2015-05-28 14:50:54 +0100

    change script to use the updated rates from genome-wide estimation

--- scripts/autozygosity/plot_autozygpsotiy_rates.R
+++ scripts/autozygosity/plot_autozygpsotiy_rates.R
@@ -1,26 +1,72 @@
 
 library(Cairo)
+library(recessiveStats)
+library(dplyr)
 
-ALL_PROBANDS_PATH = "/nfs/users/nfs_j/jm33/apps/recessiveStats/results/autozygosity.all_genes.all_probands.bjob_output.txt"
-CONSANG_PROBANDS_PATH = "/nfs/users/nfs_j/jm33/apps/recessiveStats/results/autozygosity.all_genes.consanguinous_probands.bjob_output.txt"
+AUTOZYGOSITY_DIR = "/nfs/users/nfs_j/jm33/apps/recessiveStats/data-raw/autozygosity"
+KINSHIP_PATH = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2014-11-04/kinship_and_pca_trios.txt"
 
-get_autozygosity <- function(path) {
-    # read the autozygosity output (which is a bjob output file)
-    probands = scan(path, what="", sep="\n")
-    probands = probands[grepl("^autozygosity_rate", probands)]
-
-    # convert the data to a dataframe
-    autozygous = read.table(text=probands, sep="\t", header=FALSE, fill=TRUE)
-    names(autozygous) = c("temp", "hgnc", "chrom", "rate")
-    autozygous$rate = gsub("warning messages from top-level task callback 1", "", autozygous$rate)
-    autozygous$rate = gsub("Loading required package: proto", "", autozygous$rate)
-    autozygous$rate = as.numeric(as.character(autozygous$rate))
+#' identify the probands from consanguinous parents.
+get_consanguinous_probands <- function(path) {
+    cohort = read.table(path, sep="\t", header=TRUE, stringsAsFactors=FALSE)
     
-    # select the necessary columns, and order by gene synmbol
-    autozygous = autozygous[, c("hgnc", "chrom", "rate")]
-    autozygous = autozygous[order(autozygous$hgnc), ]
+    probands = cohort[cohort$king_kinship > 0, "proband_stable_id"]
     
-    return(autozygous)
+    return(probands)
+}
+
+#' read the autozygosity output (which are individual files for each proband)
+get_autozygous_regions <- function(path) {
+    paths = Sys.glob(file.path(path, "*"))
+    
+    regions = vector("list", length(paths))
+    
+    for (pos in 1:length(paths)) {
+        temp = read.table(paths[pos], sep="\t", header=TRUE,
+            colClasses=c("character", "character", "numeric", "numeric"))
+        regions[[pos]] = temp
+    }
+    
+    regions = data.frame(rbind_all(regions))
+    
+    return(regions)
+}
+
+get_autozygosity_per_gene <- function(path, regions, subset=NULL) {
+    genes = recessiveStats::gencode[recessiveStats::gencode$gene_type == "protein_coding", ]
+    genes = genes[order(genes$gene), ]
+    
+    # Define the cohort size (we can't rely upon the number of unique probands
+    # in the regions dataframe, since many probands will not have autozygous
+    # regions).
+    probands_n = length(Sys.glob(file.path(path, "*")))
+    
+    # if we want to estimate the rates for consanguinous probands only, we take
+    # a subset of the autozygous regions
+    if (!is.null(subset)) {
+        probands_n = length(subset)
+        regions = regions[regions$sample_id %in% subset, ]
+    }
+    
+    rates = data.frame(hgnc=genes$gene, chrom=genes$chr, rate=NA)
+    for (pos in 1:nrow(genes)) {
+        # get the coordinates for the gene
+        chrom = genes$chr[pos]
+        start_pos = genes$start[pos]
+        end_pos = genes$stop[pos]
+        
+        # find the autozygous regions that overlap the gene region
+        in_chrom = regions[regions$chrom == chrom, ]
+        overlapping = in_chrom[in_chrom$end_pos > start_pos
+            & in_chrom$start_pos < end_pos, ]
+        
+        rates$rate[pos] = length(unique(overlapping$sample_id))/probands_n
+    }
+    
+    # remove the chrX rates, since all of the male probands appear autozygous
+    rates = rates[rates$chrom != "X", ]
+    
+    return(rates)
 }
 
 plot_autozygosity <- function(all_probands, consang) {
@@ -49,8 +95,11 @@ plot_autozygosity <- function(all_probands, consang) {
 
 }
 
-all_autozygous = get_autozygosity(ALL_PROBANDS_PATH)
-consang_autozygous = get_autozygosity(CONSANG_PROBANDS_PATH)
+consanguinous = get_consanguinous_probands(KINSHIP_PATH)
+regions_per_proband = get_autozygous_regions(AUTOZYGOSITY_DIR)
+all_autozygous = get_autozygosity_per_gene(AUTOZYGOSITY_DIR, regions_per_proband)
+consang_autozygous = get_autozygosity_per_gene(AUTOZYGOSITY_DIR, regions_per_proband, subset=consanguinous)
+
 write.table(all_autozygous, file="autozygosity.all_genes.all_probands.tsv", sep="\t", row.names=FALSE, quote=FALSE)
 write.table(consang_autozygous, file="autozygosity.all_genes.consanguinous_probands.tsv", sep="\t", row.names=FALSE, quote=FALSE)
 
