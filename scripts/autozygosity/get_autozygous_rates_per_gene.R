d3b8090be5bb0fc508951bdea0bb7ae12285edbb
Jeremy McRae <jm33@sanger.ac.uk>
2015-06-15 10:27:30 +0100

    tweaked autozygosity rates by checking against number of families, rather than probands

--- scripts/autozygosity/get_autozygous_rates_per_gene.R
+++ scripts/autozygosity/get_autozygous_rates_per_gene.R
@@ -5,6 +5,7 @@ library(dplyr)
 AUTOZYGOSITY_DIR = "/nfs/users/nfs_j/jm33/apps/recessiveStats/data-raw/autozygosity"
 KINSHIP_PATH = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2014-11-04/kinship_and_pca_trios.txt"
 FAMILIES_PATH = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2014-11-04/family_relationships.txt"
+SIGNIFICANT_PATH = "/nfs/users/nfs_j/jm33/apps/recessiveStats/results/recessive.combined_tests.ver2.txt"
 
 #' identify the probands from consanguinous parents.
 get_consanguinous_probands <- function(path) {
@@ -46,9 +47,6 @@ get_independent_probands <- function(families, probands) {
     # find the family IDs for all the probands, then get the subset of probands
     # where the family ID is not a duplicate.
     family_ids = families$family_id[families$individual_id %in% probands]
-    
-    if (any(duplicated(family_ids))) { print("some sibs found") }
-    
     independent_probands = probands[!duplicated(family_ids)]
     
     return(independent_probands)
@@ -61,7 +59,8 @@ get_autozygosity_per_gene <- function(path, regions, families, subset=NULL) {
     # Define the cohort size (we can't rely upon the number of unique probands
     # in the regions dataframe, since many probands will not have autozygous
     # regions).
-    probands_n = length(Sys.glob(file.path(path, "*")))
+    # probands_n = length(Sys.glob(file.path(path, "*")))
+    probands_n = 3071
     
     # if we want to estimate the rates for consanguinous probands only, we take
     # a subset of the autozygous regions
@@ -94,38 +93,34 @@ get_autozygosity_per_gene <- function(path, regions, families, subset=NULL) {
     return(rates)
 }
 
-plot_autozygosity <- function(all_probands, consang) {
+plot_autozygosity <- function(all_probands, signif_path) {
     # exclude chrX, since males always appear autozygous, and I hadn't removed
     # them for this analysis run
     all_probands = all_probands[!all_probands$chrom == "X", ]
-    all_density = density(all_probands$rate, na.rm=TRUE)
-    consang = consang[!consang$chrom == "X", ]
-    consang_density = density(consang$rate, na.rm=TRUE)
-
-    max_y = max(all_density$y, consang_density$y)
-    max_x = max(all_density$x, consang_density$x)
-    min_x = min(all_density$x, consang_density$x)
-
+    
     # plot the autozygosity rates
     Cairo(file="autozygosity_rates.pdf", type="pdf", height=15, width=15, units="cm")
-    plot(all_density,
+    hist(log10(all_probands$rate),
         xlab="Autozygosity rate in DDD probands",
         main="autozygosity rates for autosomal genes",
-        xlim=c(min_x, max_x), ylim=c(0, max_y), las=1,
-        cex=1.3, cex.lab=1.3, cex.axis=1.3, tck=-0.03)
-    lines(consang_density, col="red")
-    legend("topright", legend=c("all probands", "consanguinous probands"),
-        col=c("black", "red"), bty="n", lwd=1)
+        xlim=c(-3.5, -1), las=1, cex=1.3, cex.lab=1.3, cex.axis=1.3, tck=-0.03)
+    
+    # also plot the rates for the most significantly associated genes, to show
+    # that they fall within the typical spectrum of autozygosity
+    signif_genes = read.table(signif_path, sep="\t", header=TRUE, stringsAsFactors=FALSE)
+    genes = head(signif_genes$hgnc, 8)
+    for (gene in genes) {
+        rate = all_probands$rate[all_probands$hgnc == gene]
+        abline(v=log10(rate), col="gray")
+    }
+    
     dev.off()
 }
 
 families = get_families(FAMILIES_PATH)
-consanguinous = get_consanguinous_probands(KINSHIP_PATH)
 regions_per_proband = get_autozygous_regions(AUTOZYGOSITY_DIR)
 all_autozygous = get_autozygosity_per_gene(AUTOZYGOSITY_DIR, regions_per_proband, families)
-consang_autozygous = get_autozygosity_per_gene(AUTOZYGOSITY_DIR, regions_per_proband, families, subset=consanguinous)
 
 write.table(all_autozygous, file="autozygosity.all_genes.all_probands.tsv", sep="\t", row.names=FALSE, quote=FALSE)
-write.table(consang_autozygous, file="autozygosity.all_genes.consanguinous_probands.tsv", sep="\t", row.names=FALSE, quote=FALSE)
 
-plot_autozygosity(all_autozygous, consang_autozygous)
+plot_autozygosity(all_autozygous, SIGNIFICANT_PATH)
