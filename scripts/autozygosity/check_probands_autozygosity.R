ae6736e31395ff3e1a68f9ee1b6c24c7134c09ff
Jeremy McRae <jm33@sanger.ac.uk>
2015-10-15 16:53:28 +0100

    cleaned up scripts that call autozygous regions in all probands

--- scripts/autozygosity/check_probands_autozygosity.R
+++ scripts/autozygosity/check_probands_autozygosity.R
@@ -1,8 +1,22 @@
 #### script to identify ROH regions for all probands
 
 library(recessiveStats)
+library(argparse)
 
 DIAGNOSED_PATH = "/lustre/scratch113/projects/ddd/users/jm33/ddd_4k.diagnosed.2015-10-12.txt"
+BCF_PATH = "/lustre/scratch113/projects/ddd/users/jm33/ddd_4k.bcftools.bcf"
+
+get_options <- function() {
+    parser = ArgumentParser()
+    parser$add_argument("--bcf", default=BCF_PATH, help="Path to bcf to analyse.")
+    parser$add_argument("--diagnosed", default=DIAGNOSED_PATH,
+        help="Include path to table listing probands with diagnoses, if you want
+            to exclude these probands.")
+
+    args = parser$parse_args()
+    
+    return args
+}
 
 get_bjobs <- function() {
     args = c("-o", "\"JOBID", "USER", "STAT", "QUEUE", "JOB_NAME", "delimiter=';'\"")
@@ -31,31 +45,40 @@ get_proband_ids <- function() {
     return(sort(unique(probands$individual_id)))
 }
 
-get_undiagnosed_sanger_ids <- function() {
+get_undiagnosed_sanger_ids <- function(diagnosed_path) {
     probands = get_proband_ids()
     
     # remove the probands with diagnoses, or likely diagnostic variants.
-    diagnosed = read.table(DIAGNOSED_PATH, sep="\t", header=TRUE, stringsAsFactors=FALSE)
+    diagnosed = read.table(diagnosed_path, sep="\t", header=TRUE, stringsAsFactors=FALSE)
     probands = probands[!probands %in% diagnosed$person_id]
     
     return(probands)
 }
 
-probands = get_proband_ids()
-for (proband in probands) {
+main <- function() {
     
-    while (nrow(get_bjobs()) > 500) { Sys.sleep(30) }
+    args = get_options()
     
-    command = "bsub"
-    args = c("-o", "./autozygosity.bjob_output.txt",
-        "-q", "normal",
-        "-R", "\"select[mem>100] rusage[mem=100]\"",
-        "-M", 100,
-        "bash", "-c",
-        "\"/software/R-3.1.2/bin/Rscript", "./scripts/autozygosity/proband_autozygosity.R",
-        "--proband", proband,
-        "\"")
+    if is.null(args$diagnosed) {
+        probands = get_proband_ids()
+    } else {
+        probands = get_undiagnosed_sanger_ids(args$diagnosed)
+    }
     
-    system2(command, args)
-    Sys.sleep(0.25)
+    for (proband in probands) {
+        
+        while (nrow(get_bjobs()) > 100) { Sys.sleep(30) }
+        
+        command = "bsub"
+        args = c("-o", "./get_autozygosity.bjob",
+            "bash", "-c",
+            "\"/software/R-3.2.2/bin/Rscript", "./scripts/autozygosity/proband_autozygosity.R",
+            "--proband", proband,
+            "\"")
+        
+        system2(command, args)
+        Sys.sleep(0.25)
+    }
 }
+
+main()
--- scripts/autozygosity/proband_autozygosity.R
+++ scripts/autozygosity/proband_autozygosity.R
@@ -3,11 +3,17 @@
 library(recessiveStats)
 library(argparse)
 
-parser = ArgumentParser()
-parser$add_argument("--proband", help="Proband to analyse.")
+main <- function() {
+    parser = ArgumentParser()
+    parser$add_argument("--proband", help="Proband to analyse.")
+    parser$add_argument("--bcf", help="Path to bcf to analyse.")
 
-args = parser$parse_args()
+    args = parser$parse_args()
 
-vcf = "/lustre/scratch113/projects/ddd/users/jm33/ddd_4k.bcftools.vcf.gz"
-proband_roh = check_sample_autozygosity_genome_wide(vcf, args$proband)
-write.table(proband_roh, file=file.path("data-raw", "autozygosity", args$proband), sep="\t", row.names=FALSE, quote=FALSE)
+    proband_roh = check_sample_autozygosity_genome_wide(args$vcf, args$proband)
+    write.table(proband_roh,
+        file=file.path("data-raw", "autozygosity", args$proband),
+        sep="\t", row.names=FALSE, quote=FALSE)
+}
+
+main()
