3b8b4b9492ff6d937ffe959da6696fbf0d47d4c1
Jeremy McRae <jm33@sanger.ac.uk>
2016-04-25 22:22:05 +0100

    shift package to bcftool 1.3 for checking autozygosity

--- R/calculate_autozygosity.R
+++ R/calculate_autozygosity.R
@@ -1,6 +1,6 @@
 # functions to assess autozygosity rates within a gene
 # Currently, these functions depend heavily upon:
-#     a) bcftools (version 1.0)
+#     a) bcftools (version 1.3)
 #     b) VCF (actually, BCF) file for probands used to discover biallelically
 #        inherited variants.
 # It should be straightforward to adapt the code to define a different set of
@@ -18,11 +18,10 @@
 check_sample_autozygosity_genome_wide <- function(bcf_path, proband) {
     command = BCFTOOLS
     args = c("roh",
-        "--biallelic-sites",
-        "--estimate-AF", "all",
+        "--estimate-AF", "-",
         "--GTs-only", 30,
         "--skip-indels",
-        "--samples", proband,
+        "--sample", proband,
         bcf_path)
     
     # analyse ROH and get results for the proband
@@ -33,17 +32,17 @@ check_sample_autozygosity_genome_wide <- function(bcf_path, proband) {
     # have CNV calls, which would not make it through to the merged VCF, which
     # only contains biallelic SNVs/indels)
     if (class(roh) == "try-error") {
-        roh = data.frame("sample_id"=character(0), "chrom"=character(0),
-            "pos"=numeric(0), "p_value"=numeric(0), "state"=numeric(0))
+        roh = data.frame("chrom"=character(0), "pos"=numeric(0),
+            "state"=numeric(0), "score"=numeric(0))
     }
     
-    names(roh) = c("sample_id", "chrom", "pos", "p_value", "state")
+    names(roh) = c("chrom", "pos", "state", "score")
     
     # if there aren't any ROH regions predicted in the output, then return a
     # blank dataframe
     if (!any(roh$state == 1)) {
-        return(data.frame("sample_id"=character(0), "chrom"=character(0),
-            "start_pos"=numeric(0), "end_pos"=numeric(0)))
+        return(data.frame("chrom"=character(0), "start_pos"=numeric(0),
+            "end_pos"=numeric(0)))
     }
     
     # identify the start and end positions of ROH ranges
@@ -53,8 +52,7 @@ check_sample_autozygosity_genome_wide <- function(bcf_path, proband) {
     ends = c(row_positions[ends], row_positions[length(row_positions)])
     
     # define a table of ROH ranges
-    coords = data.frame("sample_id"=roh$sample_id[starts],
-        "chrom"=roh$chrom[starts], "start_pos"=roh$pos[starts],
+    coords = data.frame("chrom"=roh$chrom[starts], "start_pos"=roh$pos[starts],
         "end_pos"=roh$pos[ends])
     
     return(coords)
--- data-raw/constants.R
+++ data-raw/constants.R
@@ -3,7 +3,7 @@
 DATAFREEZE_DIR = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2015-04-13"
 DDD_VCFS_DIR = "/lustre/scratch114/projects/ddd/release/20140912/final"
 PHENOTYPES_PATH = file.path(DATAFREEZE_DIR, "phenotypes_and_patient_info.txt")
-BCFTOOLS = "/software/hgi/pkglocal/bcftools-1.0/bin/bcftools"
+BCFTOOLS = "/software/hgi/pkglocal/bcftools-1.3/bin/bcftools"
 
 devtools::use_data(DATAFREEZE_DIR, PHENOTYPES_PATH, DDD_VCFS_DIR, BCFTOOLS,
     internal=TRUE, overwrite=TRUE)
